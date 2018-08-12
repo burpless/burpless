1524b8462449e077fad650f36fd67de0b1f0d997
Robert Coltheart <robert.coltheart@gmail.com>
2018-08-12 20:23:16 +0800

    Add visual studio addin

---
+++ src/Burpless.VisualStudio/BurplessPackage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace Burpless.VisualStudio
+{
+    [Guid("2bbd68d8-1a5f-4671-b70f-bd507dc95e91")]
+    [PackageRegistration(AllowsBackgroundLoading = true, UseManagedResourcesOnly = true)]
+    public class BurplessPackage : AsyncPackage
+    {
+        protected override Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
