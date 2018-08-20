3741c010fd9ef584dd8711bd1acc4aa1d8f8e521
Robert Coltheart <robert.coltheart@gmail.com>
2018-08-20 20:08:21 +0800

    Fix loading addin

--- src/Burpless.VisualStudio/AssemblyProperties.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using Microsoft.VisualStudio.Shell;
-
-[assembly: ProvideCodeBase(AssemblyName = "Burpless.VisualStudio")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(false)]
